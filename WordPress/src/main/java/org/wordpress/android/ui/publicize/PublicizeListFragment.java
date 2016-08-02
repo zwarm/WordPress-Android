27b11c21b71dddad3e80b47e188287cd6d4c3f0a
Will Kwon <will@kwon.co>
2016-08-02 14:41:29 -0500

    Importing Activity

--- WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeListFragment.java
+++ WordPress/src/main/java/org/wordpress/android/ui/publicize/PublicizeListFragment.java
@@ -1,5 +1,6 @@
 package org.wordpress.android.ui.publicize;
 
+import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
