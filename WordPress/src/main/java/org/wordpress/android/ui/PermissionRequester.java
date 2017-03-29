75d52385ba7997cab4f8f311e6e6d62ab5c4d34b
Will Kwon <will@kwon.co>
2017-03-29 15:48:49 -0700

    Adding PermissionRequester
    This will be the new way for users to ask for permission

---
+++ WordPress/src/main/java/org/wordpress/android/ui/PermissionRequester.java
@@ -0,0 +1,53 @@
+package org.wordpress.android.ui;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+
+import org.wordpress.android.ui.prefs.AppPrefs;
+import org.wordpress.android.util.PermissionUtils;
+
+/**
+ * Created by will on 3/29/17.
+ */
+
+public class PermissionRequester {
+    private Activity mActivity;
+
+    public PermissionRequester(Activity activity) {
+        if (activity == null) {
+            mActivity = activity;
+        }
+    }
+
+    public void showAndRequestCameraPermission() {
+        if (AppPrefs.hasCameraPermissionBeenShown()) {
+            if (PermissionUtils.checkAndRequestCameraAndStoragePermissions(mActivity, 1)) {
+
+            }
+        } else {
+            showCameraSoftAsk();
+        }
+    }
+
+    private void showCameraSoftAsk() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(mActivity);
+        builder.setMessage("Listen here noob, we're gonna need permission, k?");
+        builder.setTitle("LET US IN");
+        builder.setPositiveButton("K, brah", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                AppPrefs.setCameraPermissionShown(true);
+                showAndRequestCameraPermission();
+            }
+        });
+        builder.setNegativeButton("Nah, bruh", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+    }
+}
